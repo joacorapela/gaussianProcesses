e999e91caa4274f2a03e5a0042ad063b4d988d42
Joaquin Rapela <joacorapela@gmail.com>
2019-06-28 19:49:02 +0100

    added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(name='Gaussian Processes',
+      version='0.0.1',
+      description='Code for estimating Gaussian Processes models',
+      author='Joaquin Rapela',
+      author_email='joacorapela@gmail.com',
+      url='https://sccn.ucsd.edu/~rapela',
+      packages=find_packages(),
+     )
